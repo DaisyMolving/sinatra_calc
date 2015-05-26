7f2c30b29117929298e249043caf2fe2d6862a95
Daisy Molving <daisymolving@gmail.com>
2015-05-27 05:37:10 +1200

    completed sqrt, power and bmi calculators

--- main.rb
+++ main.rb
@@ -15,13 +15,44 @@ get '/' do
     when '*' then @first * @second
     end
   end
-
- erb :home
+  erb :home
 end
 
 get '/power' do
+  @first = params[:first].to_f
+  @second = params[:second].to_f 
+  @result = @first ** @second
+  erb :power
+end
+
+get '/sqrt' do
+  @first = params[:first].to_f
+  @result = Math.sqrt(@first)
+  erb :sqrt
+end
+
+get '/bmi' do
+  @measuring_system = params[:measuring_system]
+  if @measuring_system
+    @first = params[:first].to_f
+    @second = params[:second].to_f
+
+    @result = case @measuring_system
+    when 'm' then (@first / (@second**2)).round(2)
+    when 'i' then ((@first / (@second**2))*703).round(2)
+    end
+  end
+  erb :bmi
+end
+
+get '/mortgage' do
    
 
 
- erb :home
+ erb :mortgage
+end
+
+get '/trip_calc' do
+
+erb :trip
 end
